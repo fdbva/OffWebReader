66bef67816292e373dea78912526091b999b53c1
fdbva <villela.felipe@gmail.com>
2015-09-21 16:05:39 -0300

    query reformulada e estrutura de input feita

--- app.js
+++ app.js
@@ -17,16 +17,36 @@ var Greeter = (function () {
     };
     return Greeter;
 })();
+var InputUrl = (function () {
+    function InputUrl(element) {
+        var _this = this;
+        this.element = element;
+        this.storyLink = document.createElement('input');
+        this.storyLink.id = "storylink";
+        this.storyLink.value = "https://www.fictionpress.com/s/2961893/1/Mother-of-Learning";
+        this.goButton = document.createElement('button');
+        this.goButton.id = "gobutton";
+        this.goButton.onclick = function (e) { _this._onclick(); };
+        this.goButton.innerHTML += "GO";
+        this.element.appendChild(this.storyLink);
+        this.element.appendChild(this.goButton);
+        this.linkText = this.storyLink.value;
+    }
+    InputUrl.prototype._onclick = function () {
+        this.linkText = document.getElementById('storylink').value.toString();
+    };
+    return InputUrl;
+})();
 var Scraper = (function () {
     function Scraper(element) {
         this.element = element;
         this.element.innerHTML += "The scraping result is: ";
     }
-    Scraper.prototype.scrape = function (url) {
+    Scraper.prototype.scrape = function (link) {
         var self = this;
         $.ajax({
             type: 'GET',
-            url: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D'https%3A%2F%2Fwww.fictionpress.com%2Fs%2F2961893%2F1%2FMother-of-Learning'%20and%20xpath%3D'%2F%2F*%5B%40id%3D%22storytext%22%5D'",
+            url: link,
             dataType: 'xml',
             success: function (data) {
                 if (data) {
@@ -39,13 +59,18 @@ var Scraper = (function () {
     return Scraper;
 })();
 var YQLFormatter = (function () {
-    function YQLFormatter(urlInput) {
-        this._yqlBaseUri = "http://query.yahooapis.com/v1/yql";
+    function YQLFormatter(urlInput, xpath) {
+        this._yqlBaseUri = "https://query.yahooapis.com/v1/public/yql?";
         this._yqlQuery = "select * from html where url='" + urlInput + "'";
+        if (xpath != null) {
+            this._xPath = " and xpath=\"" + xpath + "\"";
+            this._yqlQuery += this._xPath;
+        }
+        this._yqlQuery = this._yqlBaseUri + "q=" + encodeURIComponent(this._yqlQuery);
     }
     Object.defineProperty(YQLFormatter.prototype, "yqlQuery", {
         get: function () {
-            return this._yqlBaseUri + "" + this._yqlQuery;
+            return this._yqlQuery;
         },
         enumerable: true,
         configurable: true
@@ -53,14 +78,26 @@ var YQLFormatter = (function () {
     return YQLFormatter;
 })();
 window.onload = function () {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
+    var inputHook = document.getElementById('inputurl');
+    var greeterHook = document.getElementById('greeter');
+    var inputUrl = new InputUrl(inputHook);
+    console.log(inputUrl.linkText);
+    var greeter = new Greeter(greeterHook);
     greeter.start();
-    var ele2 = document.getElementById('yqlResponse');
-    var scraper = new Scraper(ele2);
-    var queryString = new YQLFormatter("https://www.fictionpress.com/s/2961893/1/Mother-of-Learning");
-    //scraper.scrape(queryString.yqlQuery);
-    scraper.scrape('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22google.com%22&format=json&diagnostics=true&callback=');
+    var button = document.getElementById('gobutton');
+    button.addEventListener('click', function (e) {
+        e.preventDefault(); // stop the form submission.
+        console.log(inputUrl.linkText);
+        inputUrl.linkText = document.getElementById('storylink').value.toString();
+        console.log(inputUrl.linkText);
+        var ele2 = document.getElementById('yqlResponse');
+        ele2.innerText = "";
+        var scraper = new Scraper(ele2);
+        var queryString = new YQLFormatter(inputUrl.linkText, "//*[@id='storytext']");
+        scraper.scrape(queryString.yqlQuery);
+    });
+    //scraper.scrape("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D'https%3A%2F%2Fwww.fictionpress.com%2Fs%2F2961893%2F1%2FMother-of-Learning'%20and%20xpath%3D'%2F%2F*%5B%40id%3D%22storytext%22%5D'");
+    //                https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%27https%3A%2F%2Fwww.fictionpress.com%2Fs%2F2961893%2F1%2FMother-of-Learning%27%20and%20xpath%3D%27%2F%2F*%5B%40id%3D%27storytext%27%5D%27
     //select * from html where url='https://www.fictionpress.com/s/2961893/1/Mother-of-Learning' and xpath='//*[@id="storytext"]'
 };
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -23,13 +23,35 @@ class Greeter {
     }
 
 }
+class InputUrl {
+    element: HTMLElement;
+    storyLink: HTMLInputElement;
+    goButton: HTMLButtonElement;
+    linkText: string;
 
+    constructor(element: HTMLElement) {
+        this.element = element;
+        this.storyLink = document.createElement('input');
+        this.storyLink.id = "storylink";
+        this.storyLink.value = "https://www.fictionpress.com/s/2961893/1/Mother-of-Learning";
+        this.goButton = document.createElement('button');
+        this.goButton.id = "gobutton";
+        this.goButton.onclick = (e) => { this._onclick(); };
+        this.goButton.innerHTML += "GO";
+        this.element.appendChild(this.storyLink);
+        this.element.appendChild(this.goButton);
+        this.linkText = this.storyLink.value;
+    }
+    _onclick() {
+        this.linkText = (<HTMLInputElement>document.getElementById('storylink')).value.toString();
+    }
+}
 class Scraper {
     private element: HTMLElement;
     public resultJson: any;
     public result: any;
     private http: any;
-    span: HTMLElement;
+    span: HTMLSpanElement;
 
 
     constructor(element: HTMLElement) {
@@ -37,11 +59,11 @@ class Scraper {
         this.element.innerHTML += "The scraping result is: ";
     }
 
-    scrape(url: string) {
+    scrape(link: string) {
         var self = this;
         $.ajax({
             type: 'GET',
-            url: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D'https%3A%2F%2Fwww.fictionpress.com%2Fs%2F2961893%2F1%2FMother-of-Learning'%20and%20xpath%3D'%2F%2F*%5B%40id%3D%22storytext%22%5D'",
+            url: link,
             dataType: 'xml',
             success: data => {
 
@@ -57,28 +79,47 @@ class Scraper {
 class YQLFormatter {
     private _yqlBaseUri: string;
     private _yqlQuery: string;
+    private _xPath: string;
 
-    constructor(urlInput: string) {
-        this._yqlBaseUri = "http://query.yahooapis.com/v1/yql";
+    constructor(urlInput: string, xpath : string) {
+        this._yqlBaseUri = "https://query.yahooapis.com/v1/public/yql?";
         this._yqlQuery = `select * from html where url='${urlInput}'`;
+        if (xpath != null) {
+            this._xPath = ` and xpath="${xpath}"`;
+            this._yqlQuery += this._xPath;
+        }
+        this._yqlQuery = this._yqlBaseUri + "q=" + encodeURIComponent(this._yqlQuery);
     }
 
     get yqlQuery(): string {
-        return this._yqlBaseUri + "" + this._yqlQuery;
+        return this._yqlQuery;
     }
 
 }
 
 window.onload = () => {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
+    var inputHook = document.getElementById('inputurl');
+    var greeterHook = document.getElementById('greeter');
+    var inputUrl = new InputUrl(inputHook);
+    console.log(inputUrl.linkText);
+    var greeter = new Greeter(greeterHook);
     greeter.start();
 
-    var ele2 = document.getElementById('yqlResponse');
-    var scraper = new Scraper(ele2);
-    var queryString = new YQLFormatter("https://www.fictionpress.com/s/2961893/1/Mother-of-Learning");
-    //scraper.scrape(queryString.yqlQuery);
-    scraper.scrape('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22google.com%22&format=json&diagnostics=true&callback=');
-    
+
+    var button = document.getElementById('gobutton');
+    button.addEventListener('click', function (e) {
+        e.preventDefault(); // stop the form submission.
+        console.log(inputUrl.linkText);
+        inputUrl.linkText = (<HTMLInputElement>document.getElementById('storylink')).value.toString();
+        console.log(inputUrl.linkText);
+        var ele2 = document.getElementById('yqlResponse');
+        ele2.innerText = "";
+        var scraper = new Scraper(ele2);
+        var queryString = new YQLFormatter(inputUrl.linkText, "//*[@id='storytext']");
+        scraper.scrape(queryString.yqlQuery);
+    });
+
+    //scraper.scrape("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D'https%3A%2F%2Fwww.fictionpress.com%2Fs%2F2961893%2F1%2FMother-of-Learning'%20and%20xpath%3D'%2F%2F*%5B%40id%3D%22storytext%22%5D'");
+    //                https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%27https%3A%2F%2Fwww.fictionpress.com%2Fs%2F2961893%2F1%2FMother-of-Learning%27%20and%20xpath%3D%27%2F%2F*%5B%40id%3D%27storytext%27%5D%27
     //select * from html where url='https://www.fictionpress.com/s/2961893/1/Mother-of-Learning' and xpath='//*[@id="storytext"]'
 };
